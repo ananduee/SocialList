7c6f9c213d0bd790d000e1a5a0408b29032d17a4
Anand Mohan Sinha <anasinha@amazon.com>
2019-12-24 19:50:58 +0530

    use a html template file for serving server content

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 require("@babel/register"); // For transforming javascript on the fly
+const fs = require("fs");
 const express = require('express');
 const ViewStream = require('./src/views/ViewStream');
 
 const app = express()
 const port = 3000
 
+const baseViewText = fs.readFileSync("src/views/index.html").toString();
+const baseViewTextParts = baseViewText.split("not rendered");
+
 app.get('/', (req, res) => {
+    res.write(baseViewTextParts[0]);
     const viewStream = ViewStream.get();
-    viewStream.pipe(res)    
+    viewStream.pipe(res, { end: false });
+    viewStream.on("end", () => {
+        res.write(baseViewTextParts[1]);
+        res.end();
+    });
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
