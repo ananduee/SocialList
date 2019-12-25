77ef8659ba25a01cc2de9397cbe5cbe7d1bdd5fe
Anand Mohan Sinha <anasinha@amazon.com>
2019-12-25 13:28:14 +0530

    added flag for devo and production build

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 export default function App(props) {
-  return <p>Hello world this is some change and more change check now.</p>;
+  return (
+    <div>
+      <p>Hello world this is some change and more change check now.</p>
+      <Button>This is button more.</Button>
+    </div>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,19 @@ const path = require("path");
 const webpack = require("webpack");
 const NodemonPlugin = require("nodemon-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/views/Browser.js",
+const isDevelopment = process.env.NODE_ENV == "development";
+
+const webpackConfig = {
+  mode: isDevelopment ? "development" : "production",
+  entry: {
+    app: "./src/views/Browser.js"
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/dist",
-    filename: "app.[hash].js"
+    filename: "[name].[contenthash:8].js"
   },
   module: {
     rules: [
@@ -19,21 +24,48 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
       }
     ]
   },
   plugins: [
-    new NodemonPlugin({
-      watch: path.resolve("./"),
-      ignore: ["node_modules/*", "dist/*"],
-      script: "./index.js",
-      ext: "js,html"
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(
+        isDevelopment ? "development" : "production"
+      )
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({ template: "./src/views/index.html" })
-  ],
-  watch: true,
+    isDevelopment &&
+      new NodemonPlugin({
+        watch: path.resolve("./"),
+        ignore: ["node_modules/*", "dist/*"],
+        script: "./index.js",
+        ext: "js,html"
+      }),
+    !isDevelopment &&
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash:8].css",
+        chunkFilename: "[name].[contenthash:8].chunk.css"
+      }),
+    new HtmlWebpackPlugin({
+      template: "./src/views/index.html",
+      minify: !isDevelopment,
+      chunks: ["app"]
+    })
+  ].filter(Boolean),
+  watch: isDevelopment,
   watchOptions: {
     ignored: /node_modules/
   }
 };
+
+if (!isDevelopment) {
+  webpackConfig.devtool = "nosource-source-map";
+}
+
+module.exports = webpackConfig;
